cc9b79f06eb839a544bf28a8e5bfcd6891b7c30f
Hevar Djeza <hevar@live.se>
2025-11-14 15:51:57 +0100

    Scienced

--- scripts/big_ball.gd
+++ scripts/big_ball.gd
@@ -2,5 +2,10 @@ extends RigidBody2D
 
 @export var initial_velocity: Vector2
 
+const min_x_speed = 200
+
 func _ready():
 	linear_velocity = initial_velocity
+
+func _physics_process(delta: float) -> void:
+	linear_velocity.x = sign(linear_velocity.x) * max(abs(linear_velocity.x), min_x_speed)
