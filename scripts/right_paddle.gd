90a77bd0b04e51c1b90b63165abebab305d75833
Hevar Djeza <hevar@live.se>
2025-11-14 14:37:10 +0100

    Dual-paddle technology

---
+++ scripts/right_paddle.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+@onready var color_rect = $ColorRect
+@onready var collision_shape = $CollisionShape2D
+
+const paddle_speed = 300
+
+func _ready() -> void:
+	var color_size = color_rect.size * color_rect.scale
+	collision_shape.shape.extents = color_size / 2
+	collision_shape.global_position += color_size / 2
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("RightPaddleUp"):
+		global_position.y -= paddle_speed * delta
+	if Input.is_action_pressed("RightPaddleDown"):
+		global_position.y += paddle_speed * delta
+	
